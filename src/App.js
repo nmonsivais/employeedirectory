f5dd068882a01125fa9027ac748be4631e0f5b93
Tro <noe.monsivais@gmail.com>
2020-06-25 12:01:08 -0500

    added function to alphabetize by last name.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import employees from "./employees.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+class App extends React.Component {
+  // Setting the initial state of the Counter component
+  state = {
+    employees: employees
+
+  };
+
+  handleLastName = () => {
+    console.log("hi")
+    this.setState({ employees: this.state.employees.sort((a, b) => { return a.employeeLName.localeCompare(b.employeeLName) }) })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <button onClick={this.handleLastName}>Last Name</button>
+
+          {
+            this.state.employees.map((employee, index) => {
+              return <div key={index}>{employee.employeeFName} {employee.employeeLName} : {employee.occupation}</div>
+            })
+
+          }
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/tutorial.js
@@ -0,0 +1,8 @@
+const colors = [{ name: 'blue' }, { name: 'red' }]
+
+colors.map((color, index, arr) => {
+
+    console.log(color)
+    console.log(index)
+    console.log('==========')
+})
